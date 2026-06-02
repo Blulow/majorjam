78d1350aa62381f81842eb9612b3c8e95235f838
Blulow <ngxinler1223@gmail.com>
2026-06-03 00:38:07 +0800

    change: wave system structure

--- scripts/spawners/enemy_spawner.gd
+++ scripts/spawners/enemy_spawner.gd
@@ -1,39 +1,26 @@
 extends Node2D
 class_name EnemySpawner
 
-@export var wave_pattern: WavePattern
 @export var arena: Node2D
+@export var interval: float
+@export var active: bool
+@export var enemy_cap: int
+@export var pool: Array[PoolData]
+@export var stat_modifier: StatModifier
 
-var active: bool
-var wave_n: int
-var duration_timer: float
 var spawn_timer: float = 0.0
-
-func _ready() -> void:
-	start_wave(preload("res://assets/resources/wave_patterns/wave_1.tres"))
-
-func start_wave(_wave_pattern: WavePattern):
-	wave_pattern = _wave_pattern
-	wave_n = wave_pattern.wave_n
-	duration_timer = wave_pattern.duration
-	active = true
 
 func _process(delta: float) -> void:
 	if not active: return
 	
-	if spawn_timer < wave_pattern.interval:
+	if spawn_timer < interval:
 		spawn_timer += delta
 	else:
-		if len(get_tree().get_nodes_in_group("enemy")) < wave_pattern.enemy_cap:
-			var weights: Array = wave_pattern.pool.map(func(e): return e.weight)
-			var pool_data: PoolData = weighted_rand(wave_pattern.pool, weights)
+		if len(get_tree().get_nodes_in_group("enemy")) < enemy_cap:
+			var weights: Array = pool.map(func(e: PoolData): return e.weight)
+			var pool_data: PoolData = weighted_rand(pool, weights)
 			spawn_enemies(pool_data.enemy_scene, pool_data.count, pool_data.spawn_pattern, pool_data.target)
 		spawn_timer = 0.0
-	
-	if duration_timer > 0:
-		duration_timer -= delta
-	else:
-		active = false
 
 func spawn_enemies(enemy_scene: PackedScene, count: int, spawn_pattern: SpawnPattern, target: NodePath):
 	var points: Array[Vector2] = spawn_pattern.get_spawn_positions(count)
@@ -41,8 +28,8 @@ func spawn_enemies(enemy_scene: PackedScene, count: int, spawn_pattern: SpawnPat
 		var enemy: Enemy = enemy_scene.instantiate()
 		enemy.target = get_tree().current_scene.get_node(target)
 		enemy.global_position = i
-		enemy.get_node("Health").health *= wave_pattern.stat_modifier.health_modifier
-		enemy.get_node("Hitbox").damage_amount *= wave_pattern.stat_modifier.damage_modifier
+		enemy.get_node("Health").health *= stat_modifier.health_modifier
+		enemy.get_node("Hitbox").damage_amount *= stat_modifier.damage_modifier
 		arena.add_child(enemy)
 
 func weighted_rand(items: Array, weights: Array):
---
+++ scripts/wave_manager.gd
@@ -0,0 +1,34 @@
+extends Node2D
+class_name WaveManager
+
+@export var wave_patterns: Array[WavePattern]
+@export var enemy_spawner: EnemySpawner
+
+var wave_pattern: WavePattern
+var wave_n: int
+var active: bool
+var duration_timer: float
+
+func _ready() -> void:
+	start_wave(preload("res://assets/resources/wave_patterns/wave_1.tres"))
+
+func _process(delta: float) -> void:
+	if not active: return
+	
+	if duration_timer > 0:
+		duration_timer -= delta
+	else:
+		active = false
+		enemy_spawner.active = false
+
+func start_wave(_wave_pattern: WavePattern):
+	wave_pattern = _wave_pattern
+	wave_n = wave_pattern.wave_n
+	duration_timer = wave_pattern.duration
+	active = true
+	
+	enemy_spawner.interval = wave_pattern.interval
+	enemy_spawner.enemy_cap = wave_pattern.enemy_cap
+	enemy_spawner.pool = wave_pattern.pool
+	enemy_spawner.stat_modifier = wave_pattern.stat_modifier
+	enemy_spawner.active = active
