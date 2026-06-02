300a4b2df86692b598efe3285a1803abda6abf15
Blulow <ngxinler1223@gmail.com>
2026-06-02 22:02:50 +0800

    fix: collision not registering

--- scripts/health/hurtbox.gd
+++ scripts/health/hurtbox.gd
@@ -9,7 +9,7 @@ var hit: bool = false
 @onready var timer: float = hit_cooldown
 
 func _process(delta: float) -> void:
-	if timer < hit_cooldown:
+	if timer <= hit_cooldown:
 		timer += delta
 	else:
 		if not hit: return
@@ -17,7 +17,6 @@ func _process(delta: float) -> void:
 		timer = 0.0
 
 func _on_area_entered(area: Area2D) -> void:
-	if area is Hitbox:
 		damage_amount = area.damage_amount
 		hit = true
 
