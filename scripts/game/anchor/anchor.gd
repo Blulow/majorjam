a27e9ae8af3d3d082c4bb2bec7f8cc5ec3bf5f11
Blulow <ngxinler1223@gmail.com>
2026-06-05 06:46:29 +0800

    fix: gameplay balancing | add: lightning sprite

--- scripts/game/anchor/anchor.gd
+++ scripts/game/anchor/anchor.gd
@@ -2,12 +2,14 @@ extends StaticBody2D
 class_name Anchor
 
 signal hit
+signal died
 
 func _on_health_died() -> void:
 	if $RopeEnd:
 		$RopeEnd.gravity_scale = 1
 		$RopeEnd.reparent(get_node(".."))
 	
+	died.emit()
 	queue_free()
 
 func _on_health_hit() -> void:
--- scripts/game/player/player_died.gd
+++ scripts/game/player/player_died.gd
@@ -6,9 +6,18 @@ extends VisibleOnScreenNotifier2D
 
 func _ready() -> void:
 	death_screen.visible = false
+	anchor.died.connect(_on_anchor_died)
 
 func _on_screen_exited() -> void:
 	if anchor: return
+	
+	show_death_screen()
+
+func _on_anchor_died() -> void:
+	if not is_on_screen():
+		show_death_screen()
+
+func show_death_screen() -> void:
 	audio.stop()
 	
 	await get_tree().create_timer(0.5).timeout
